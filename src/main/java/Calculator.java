54d962bc2f6e612defd32513a9a367cd817276f1
Aneta Prokopiuk <anetaprokopiuk@gmail.com>
2018-12-09 13:33:34 +0100

    branch genericInterface

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,7 +1,3 @@
-//public interface Calculator {
-//    String calculate(double weight, double height);
-//    String interpret(String bmi);
-//}
 public interface Calculator<T> {
     T calculate(double ...a);
     String interpret(T paramToInterpret);
